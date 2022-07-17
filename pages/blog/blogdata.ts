d71c69662692d52a237e079d30ffa8cba47fec91
humgal <humgal@outlook.com>
2022-07-17 23:08:11 +0800

    blog

---
+++ pages/blog/blogdata.ts
@@ -0,0 +1,67 @@
+export const authors = {
+    oliviertassinari: {
+      name: 'Olivier Tassinari',
+      avatar: 'https://avatars.githubusercontent.com/u/3165635',
+      github: 'oliviertassinari',
+    },
+    mbrookes: {
+      name: 'Matt Brookes',
+      avatar: 'https://avatars.githubusercontent.com/u/357702',
+      github: 'mbrookes',
+    },
+    eps1lon: {
+      name: 'Sebastian Silbermann',
+      avatar: 'https://avatars.githubusercontent.com/u/12292047',
+      github: 'eps1lon',
+    },
+    mnajdova: {
+      name: 'Marija Najdova',
+      avatar: 'https://avatars.githubusercontent.com/u/4512430',
+      github: 'mnajdova',
+    },
+    michaldudak: {
+      name: 'Michał Dudak',
+      avatar: 'https://avatars.githubusercontent.com/u/4696105',
+      github: 'michaldudak',
+    },
+    siriwatknp: {
+      name: 'Siriwat Kunaporn',
+      avatar: 'https://avatars.githubusercontent.com/u/18292247',
+      github: 'siriwatknp',
+    },
+    'danilo-leal': {
+      name: 'Danilo Leal',
+      avatar: 'https://avatars.githubusercontent.com/u/67129314',
+      github: 'danilo-leal',
+    },
+    m4theushw: {
+      name: 'Matheus Wichman',
+      avatar: 'https://avatars.githubusercontent.com/u/42154031',
+      github: 'm4theushw',
+    },
+    flaviendelangle: {
+      name: 'Flavien Delangle',
+      avatar: 'https://avatars.githubusercontent.com/u/3309670',
+      github: 'flaviendelangle',
+    },
+    DanailH: {
+      name: 'Danail Hadjiatanasov',
+      avatar: 'https://avatars.githubusercontent.com/u/5858539',
+      github: 'DanailH',
+    },
+    alexfauquette: {
+      name: 'Alexandre Fauquette',
+      avatar: 'https://avatars.githubusercontent.com/u/45398769',
+      github: 'alexfauquette',
+    },
+    samuelsycamore: {
+      name: 'Sam Sycamore',
+      avatar: 'https://avatars.githubusercontent.com/u/71297412',
+      github: 'samuelsycamore',
+    },
+    josefreitas: {
+      name: 'José Freitas',
+      avatar: 'https://avatars.githubusercontent.com/u/550141',
+      github: 'joserodolfofreitas',
+    },
+  };
